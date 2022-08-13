b4e65efeff915f218794ec5ac9ed8f69db24b0c7
Felix Damrau <mail@felixdamrau.de>
2022-08-13 19:30:58 +0200

    Add simple "get pokemon" page

---
+++ src/Develix.Pokemons.State/PokedexUseCase/Reducers.cs
@@ -0,0 +1,23 @@
+﻿namespace Develix.Pokemons.State.PokedexUseCase;
+
+public static class Reducers
+{
+    [ReducerMethod(typeof(GetPokemonAction))]
+    public static PokedexState ReduceGetPokemonAction(PokedexState state)
+    {
+        return state with
+        {
+            IsLoading = true,
+        };
+    }
+
+    [ReducerMethod]
+    public static PokedexState ReduceGetPokemonResultAction(PokedexState state, GetPokemonResultAction action)
+    {
+        return state with
+        {
+            Pokemon = action.Pokemon,
+            IsLoading = false,
+        };
+    }
+}
--- src/Develix.Pokemons.UI.Blazor/Program.cs
+++ src/Develix.Pokemons.UI.Blazor/Program.cs
@@ -1,13 +1,17 @@
+﻿using Develix.Pokemons.State.PokedexUseCase;
+using Develix.Pokemons.UI.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Develix.Pokemon.UI.Blazor;
 using MudBlazor.Services;
+using PokeApiNet;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddFluxor(op => op.ScanAssemblies(typeof(PokedexState).Assembly));
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<PokeApiClient>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
