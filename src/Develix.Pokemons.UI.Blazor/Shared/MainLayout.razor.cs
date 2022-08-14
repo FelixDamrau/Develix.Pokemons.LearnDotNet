65ade8d0401b4bcccf8c727e35cd60ec248604f1
Felix Damrau <mail@felixdamrau.de>
2022-08-14 13:41:13 +0200

    Cleanup (#2)
    * Add solution level files
    * Update github repo URI
    * Add version info in MainLayout
    * Update README

--- src/Develix.Pokemons.UI.Blazor/Shared/MainLayout.razor.cs
+++ src/Develix.Pokemons.UI.Blazor/Shared/MainLayout.razor.cs
@@ -1,11 +1,12 @@
-﻿namespace Develix.Pokemons.UI.Blazor.Shared;
+﻿using System.Reflection;
+
+namespace Develix.Pokemons.UI.Blazor.Shared;
 
 public partial class MainLayout
 {
     private bool drawerOpen = true;
 
-    private void DrawerToggle()
-    {
-        drawerOpen = !drawerOpen;
-    }
+    private void DrawerToggle() => drawerOpen = !drawerOpen;
+
+    private static string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown version";
 }
